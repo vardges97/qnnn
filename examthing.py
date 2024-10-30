0f35848ea0e18e10a13d2380b0bb81d9a5d996a4
vardges <107487055+vardges97@users.noreply.github.com>
2024-10-30 10:45:54 +0400

    Add files via upload

--- examthing.py
+++ examthing.py
@@ -20,60 +20,60 @@
 # # # # # #   def log(self, message):
 # # # # # #     pass
 
-class our_property:
-    """ emulation of the property class
-        for educational purposes """
-
-    def __init__(self,
-                 fget=None,
-                 fset=None,
-                 fdel=None,
-                 doc=None):
-        """Attributes of 'our_decorator'
-        fget
-            function to be used for getting
-            an attribute value
-        fset
-            function to be used for setting
-            an attribute value
-        fdel
-            function to be used for deleting
-            an attribute
-        doc
-            the docstring
-        """
-        self.fget = fget
-        self.fset = fset
-        self.fdel = fdel
-        if doc is None and fget is not None:
-            doc = fget.__doc__
-        self.__doc__ = doc
-
-    def __get__(self, obj, objtype=None):
-        if obj is None:
-            return self
-        if self.fget is None:
-            raise AttributeError("unreadable attribute")
-        return self.fget(obj)
-
-    def __set__(self, obj, value):
-        if self.fset is None:
-            raise AttributeError("can't set attribute")
-        self.fset(obj, value)
-
-    def __delete__(self, obj):
-        if self.fdel is None:
-            raise AttributeError("can't delete attribute")
-        self.fdel(obj)
-
-    def getter(self, fget):
-        return type(self)(fget, self.fset, self.fdel, self.__doc__)
-
-    def setter(self, fset):
-        return type(self)(self.fget, fset, self.fdel, self.__doc__)
-
-    def deleter(self, fdel):
-        return type(self)(self.fget, self.fset, fdel, self.__doc__)
+# class our_property:
+#     """ emulation of the property class
+#         for educational purposes """
+#
+#     def __init__(self,
+#                  fget=None,
+#                  fset=None,
+#                  fdel=None,
+#                  doc=None):
+#         """Attributes of 'our_decorator'
+#         fget
+#             function to be used for getting
+#             an attribute value
+#         fset
+#             function to be used for setting
+#             an attribute value
+#         fdel
+#             function to be used for deleting
+#             an attribute
+#         doc
+#             the docstring
+#         """
+#         self.fget = fget
+#         self.fset = fset
+#         self.fdel = fdel
+#         if doc is None and fget is not None:
+#             doc = fget.__doc__
+#         self.__doc__ = doc
+#
+#     def __get__(self, obj, objtype=None):
+#         if obj is None:
+#             return self
+#         if self.fget is None:
+#             raise AttributeError("unreadable attribute")
+#         return self.fget(obj)
+#
+#     def __set__(self, obj, value):
+#         if self.fset is None:
+#             raise AttributeError("can't set attribute")
+#         self.fset(obj, value)
+#
+#     def __delete__(self, obj):
+#         if self.fdel is None:
+#             raise AttributeError("can't delete attribute")
+#         self.fdel(obj)
+#
+#     def getter(self, fget):
+#         return type(self)(fget, self.fset, self.fdel, self.__doc__)
+#
+#     def setter(self, fset):
+#         return type(self)(self.fget, fset, self.fdel, self.__doc__)
+#
+#     def deleter(self, fdel):
+#         return type(self)(self.fget, self.fset, fdel, self.__doc__)
 
 
 # better 6 # class Attributes:
@@ -100,3 +100,64 @@ class our_property:
 # #       else:
 # #         print(f"- Class attribute: {attr_name}")
 # #     print(f"- Instance attributes: {list(instance_attributes)}")
+
+
+# Creating a class dynamically using type()
+Car = type('car', (object,), {'brand': lambda self: "lancia",'model': lambda self: "stratos"})
+
+# Using the dynamically created class
+# instance = Car()
+# print(instance.brand())
+# print(instance.model())
+
+
+# class First(object):
+#     def __init__(self):
+#         print ("first")
+#
+# class Second(object):
+#     def __init__(self):
+#         print ("second")
+#
+# class Third(First, Second):
+#     def __init__(self):
+#         First.__init__(self)
+#         Second.__init__(self)
+#         print( "that's it")
+
+# t = Third()
+
+class First(object):
+    def __init__(self):
+        super(First, self).__init__()
+        print("first")
+class Second(object):
+    def __init__(self):
+        super(Second, self).__init__()
+        print("second")
+class Third(Second,First):
+    def __init__(self):
+        super(Third, self).__init__()
+        print("third")
+
+class A:
+    def __init__(self):
+        print('A')
+        super().__init__()
+
+class B(A):
+    def __init__(self):
+        print('B')
+        super().__init__()
+class X:
+    def __init__(self):
+        print('X')
+        super().__init__()
+
+class Forward(B,A,X):
+    def __init__(self):
+        print('Forward')
+        super().__init__()
+
+Forward()
+Third()
